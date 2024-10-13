08f8fb183957991da1e59ef5be0acad15c21a54c
asdrew-wq <ndrwromaniuk@gmail.com>
2024-10-13 19:42:01 +0300

    Змінено функцію f, створено програму по завданню

--- task1/task.py
+++ task1/task.py
@@ -1,12 +1,22 @@
-def f(a, b, c):
-    return a, b, c
+def f(sides):
+    if sides[0] ** 2 + sides[1] ** 2 == sides[2] ** 2:
+        return True
+    return False
 
 
 if __name__ == '__main__':
     abc = input('Введіть a, b, c через пробіл: ')
+    while abc.startswith(' '):
+        abc = abc[1:]
+    while abc.endswith(' '):
+        abc = abc[:-1]
     try:
-        a, b, c = abc.split(' ')
-    except ValueError:
+        abc = [eval(i) for i in abc.split(' ')]
+        abc.sort()
+        assert len(abc) == 3
+        if f(abc):
+            print('Це сторони трикутика Піфагора')
+        else:
+            print('Це не сторони трикутика Піфагора')
+    except (TypeError, AssertionError, NameError):
         print('Потрібно ввести три числа')
-    else:
-        print(f(a, b, c))
